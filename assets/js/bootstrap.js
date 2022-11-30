90e4d31c40b7bd4e2a9db0ee58eb29fc8f296701
ytVIDA <czvidayt@gmail.com>
2022-11-30 21:54:01 +0100

    Update bootstrap.js

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -3,8 +3,9 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
-if(/https/.test(window.location.protocol)){
-  window.location.href = window.location.href.replace('http:', 'https:');    
+var loc = window.location.href+'';
+if (loc.indexOf('http://')==0){
+    window.location.href = loc.replace('http://','https://');
 }
 
 
